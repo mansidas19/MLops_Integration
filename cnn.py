918adac863c5276271064c1af46a3f9e2e4fd482
mansidas19 <62935129+mansidas19@users.noreply.github.com>
2020-05-24 16:35:57 +0530

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -1,3 +1,25 @@
-print("hi")
-x=5
-print(x)
+from keras.datasets import fashion_mnist 
+from tensorflow.keras.layers import Dense, Activation, Flatten, Conv2D, MaxPooling2D
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.utils import to_categorical
+import numpy as np
+(train_X,train_Y), (test_X,test_Y) = fashion_mnist.load_data()
+train_X = train_X.reshape(-1, 28,28, 1)
+test_X = test_X.reshape(-1, 28,28, 1)
+train_X.shape
+train_X = train_X.astype('float32')
+test_X = test_X.astype('float32')
+train_X = train_X / 255
+test_X = test_X / 255
+train_Y_one_hot = to_categorical(train_Y)
+test_Y_one_hot = to_categorical(test_Y)
+model = Sequential()
+model.add(Conv2D(64, (3,3),activation='relu', input_shape=(28, 28, 1)))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Flatten())
+model.add(Dense(64,activation='relu'))
+model.add(Dense(10,activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam',metrics=['accuracy'])
+model.fit(train_X, train_Y_one_hot, batch_size=64, epochs=1)
+test_loss, test_acc = model.evaluate(test_X, test_Y_one_hot)
+print('Test accuracy', test_acc)
